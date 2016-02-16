5de99d5030e5d4492fa53f0be16409af76bd7e2f
William Grondin <william.grondin@gmail.com>
2016-02-16 15:44:46 +0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,7 +5,6 @@ BASEDIR=$(dirname $0)
 echo -n "How many users ? [~1GB RAM/users] : "
 read nbChromeUser;
 
-rm -rf $BASEDIR/user*
 
 echo "$nbChromeUser users";
 echo "";
@@ -15,11 +14,13 @@ echo -e "Germany : de\tItaly : it\tJaan : jp\tMexico : mx\tNetherland : nl";
 echo -e "Poland : pl\tRussia : ru\tSouth africa : za\tSouth korea : kr\t"
 echo -e "Spain : es\tSweden : se\tGreatBritain: gb\tUSA: us\t"
 
+rm $BASEDIR/conutries
+
+
 for i in `seq 1 $nbChromeUser`
 do
-cp -r $BASEDIR/default-user $BASEDIR/user$i
 echo -n "Select country for user$i : "
 read country;
-sqlite3 $BASEDIR/user$i/Default/Local\ Storage/chrome-extension_gkojfkhlekighikafcpjkiklfbnlmeio_0.localstorage "UPDATE ItemTable SET value=replace(value,'ca','$country') WHERE key='be_rules';"
+echo -e "$country" >> $BASEDIR/countries
 echo "country modified"
 done
