477506d1cc682062e8f52095e4ed0076a3b2e903
William Grondin <william.grondin@gmail.com>
2016-02-29 01:13:58 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ apt-get update
 apt-get clean
 apt-get autoclean
 apt-get autoremove -y
-apt-get install -y psmisc xvfb gtk2-engines-pixbuf libexif12 libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xfonts-cyrillic x11-apps chromium-browser
+apt-get install -y psmisc xvfb gtk2-engines-pixbuf libexif12 libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xfonts-cyrillic x11-apps chromium-browser sqlite
